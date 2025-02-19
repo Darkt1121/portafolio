b5cfa170306b013251c913992a0dd6ee45a0c935
kevin rigs dionicio sanchez <97697147+Darkt1121@users.noreply.github.com>
2025-02-19 14:17:56 -0300

    yes

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function barmobile(){
-    document.getElementById("menu").classList.toggle
-    ("active");
+// function barmobile(){
+//     document.getElementById("menu").classList.toggle
+//     ("active");
+// }
+
+
+
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
 }
 
-
-
-
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
